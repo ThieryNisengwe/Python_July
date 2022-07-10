0ae0504ef73e8a8f88c14651c5070c1b0efa455e
ThieryNisengwe <thierynisengwe@gmail.com>
2022-07-10 13:41:08 -0700

    21

---
+++ Practice_Assignments/Hello_world/hello_world.py
@@ -0,0 +1,20 @@
+from tkinter import N
+from turtle import numinput
+
+
+print("Hello World")
+name = "Thiery"
+print("Hello", name)
+name = "Nisengwe"
+print("Hello " + name)
+num = 48
+print("Hello", num)
+print("Hello " + str(num))
+
+fav_food1 = "Teryaki"
+fav_food2 = "Burger"
+print("I love to eat", fav_food1, 'and', fav_food2+'s')
+print(f"I love to eat {fav_food1} and {fav_food2}s")
+whats_nine_plus_ten = 21
+print(whats_nine_plus_ten)
+print(f"What's nine plus ten {whats_nine_plus_ten}")
