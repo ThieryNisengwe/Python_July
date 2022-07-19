b5fca2c51a6b2b46910f5fe8d13cbb4f76a9cfea
ThieryNisengwe <thierynisengwe@gmail.com>
2022-07-19 16:36:04 -0700

    moose

--- flask/counter/server.py
+++ flask/counter/server.py
@@ -1,33 +1,31 @@
-from crypt import methods
-import re
-from flask import Flask,render_template,request,redirect,session
-app = Flask(__name__)
-app.secret_key = 'counter'
+from flask import Flask,render_template,redirect,request,session
 
-@app.route('/')
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+count = 0 
+
+@app.route("/")
 def counter():
-    if 'count' not in session:
-        session['count'] = 1
-    else:
-        session['count'] += 1
-    render_template('index.html')
-    
-    
-    
-@app.route('/count', methods=['POST'])
-def view_count():
-    if request.form['alter']=='add':
-        session['count'] += 1
-    elif request.form['alter']=='reset':
-        session['count']=0
-        return redirect('/')
-    
-    
-@app.route('/destroy')
-def destroy():
-    session.clear
+    print(request.form)
+    global count
+    count += 1 
+    return render_template("index.html", count = count)
+
+@app.route('/addTwo',methods =['POST'])
+def addTwo():
+    print('GLOBAL COUNT =')
+    global count
+    count += 1
+    print(count)
     return redirect('/')
 
-if __name__=='__main__':
+@app.route('/reset',methods=['POST'])
+def reset():
+    global count
+    count = 0 
+    print(count)
+    return redirect('/')
+
+if __name__=="__main__":
     app.run(debug=True)
-    
