4d559004b0761ef0a0d0e87c8c9dc9769aa64e3e
ThieryNisengwe <thierynisengwe@gmail.com>
2022-07-19 17:27:31 -0700

    k

---
+++ flask/dojo_survey/server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = "THIERY IS MY KEY"
+
+
+@app.route('/')
+def dojo_survey():
+    return render_template("index.html")
+
+
+@app.route('/result')
+def survey_results():
+    return render_template('result.html')
+
+
+@app.route('/process', methods=['POST'])
+def process_survey():
+    print(request.form['name'], request.form['location'])
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['favorite_language'] = request.form['favorite_language']
+    session['comments'] = request.form['comments']
+    return redirect('/result')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
