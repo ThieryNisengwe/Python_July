5adfda7486842e93b61d3ea0368fb2c1ae7b56ca
thiery nisengwe <thierynisengwe@gmail.com>
2022-07-26 11:21:28 -0700

    moved directories

--- To_Learn/for_loops/for_loops.py
+++
@@ -1,2 +0,0 @@
-for number in range(3):
-    print('Ateempt', number + 1)
---
+++ pythonMac/server.py
@@ -0,0 +1,9 @@
+# from distutils.log import debug
+# from flask import flask,render_template
+
+# app - Flask(__name__)
+
+# @app.route('/')
+
+# if __name__=='__main__':
+#     app.run(debug=True)
